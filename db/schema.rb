fad8578e16cdc586a717278825b01a23c472907c
kskbass1030 <kskbass@icloud.com>
2024-02-13 01:39:20 +0000

    feature rating 5star

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_09_125041) do
+ActiveRecord::Schema.define(version: 2024_02_13_010705) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2024_02_09_125041) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score", default: 0
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
