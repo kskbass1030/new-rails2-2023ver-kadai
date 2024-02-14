65ee834e016450c0fda0f22ad7951ccad533a44c
kskbass1030 <kskbass@icloud.com>
2024-02-14 07:55:30 +0000

    feature admin dashboard 9

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -11,6 +11,8 @@ class ShoppingCart < ApplicationRecord
   scope :bought_days_mysql, -> { bought_carts.order(updated_at: :desc).group("date_format(updated_at + interval 9 hour, '%Y-%m-%d')").pluck(:updated_at) }
   scope :search_bought_carts_by_month, -> (month) { bought_carts.where(updated_at: month.all_month) }
   scope :search_bought_carts_by_day, -> (day) { bought_carts.where(updated_at: day.all_day) }
+  scope :search_carts_by_ids, -> (ids) { where("id LIKE ?", "%#{ids}%") }
+  scope :search_bought_carts_by_ids, -> (ids) { bought_carts.search_carts_by_ids(ids) }
   scope :sort_list, -> {
     {"日別": "daily", "月別": "month"}
   }
